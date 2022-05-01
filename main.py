af6b304c565be164674abf38a484ca60c86a8ea4
Suresh Patel <sjp294@rutgers.edu>
2022-04-30 20:20:44 -0400

    init

---
+++ main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+
+
+board  = ['-', '-', '-', 
+          '-', '-', '-', 
+          '-', '-', '-']
+
+def display_board():
+    print(board[0] + " | " + board[1] + " | " + board[2])
+    print(board[3] + " | " + board[4] + " | " + board[5])
+    print(board[6] + " | " + board[7] + " | " + board[8])
+
+display_board()
+ 
+# board
+# display 
+# play game
+# handle turn
+# check win
+  # Check row
+  # Check Column
+  # Check Diagnoal
+# check tie
+
+# flip between players
